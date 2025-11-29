43d2b29784475c60291a14fac79e2b7a361b5ce3
带鱼Yuiffy <yuiffy@qq.com>
2025-11-29 18:14:33 +0800

    fix: 再优化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Script from 'next/script';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,11 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7443445828476112"
           crossOrigin="anonymous"
-          strategy="beforeInteractive"
         />
       </head>
       <body className={inter.className}>
